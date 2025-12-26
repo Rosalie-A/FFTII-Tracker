261a8d53a99c49cadb359f3bdad757ff29ca2b20
Rosalie <rosalieemolina@gmail.com>
2025-12-26 11:10:21 -0500

    Enabled hint highlighting.

--- scripts/autotracking/archipelago.lua
+++ scripts/autotracking/archipelago.lua
@@ -330,8 +330,8 @@ end
 -- Archipelago:AddItemHandler("item handler", onItem)
 -- Archipelago:AddLocationHandler("location handler", onLocation)
 
--- Archipelago:AddSetReplyHandler("notify handler", onNotify)
--- Archipelago:AddRetrievedHandler("notify launch handler", onNotifyLaunch)
+Archipelago:AddSetReplyHandler("notify handler", onNotify)
+Archipelago:AddRetrievedHandler("notify launch handler", onNotifyLaunch)
 
 
 
